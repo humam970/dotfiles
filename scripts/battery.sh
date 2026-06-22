becae689176529ef918857cddeb3f6321fac21ad
HUMAM970 <humam.102001@gmail.com>
2026-06-22 22:17:36 +0300

    committed

---
+++ scripts/battery.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+BAT="/sys/class/power_supply/BAT1"
+LOW=20
+CRITICAL=10
+
+notified_low=0
+notified_critical=0
+
+while sleep 120; do
+    battery=$(<"$BAT/capacity")
+    status=$(<"$BAT/status")
+
+    if [ "$status" = "Discharging" ]; then
+        if [ "$battery" -le "$LOW" ] && [ "$notified_low" -eq 0 ]; then
+            notify-send \
+                -a Battery \
+                "Battery low" \
+                "Battery at ${battery}%"
+            notified_low=1
+        fi
+
+        if [ "$battery" -le "$CRITICAL" ] && [ "$notified_critical" -eq 0 ]; then
+            notify-send \
+                -u critical \
+                -a Battery \
+                "Battery critical" \
+                "Battery at ${battery}%"
+            notified_critical=1
+        fi
+    else
+        notified_low=0
+        notified_critical=0
+    fi
+
+    [ "$battery" -gt "$LOW" ] && notified_low=0
+    [ "$battery" -gt "$CRITICAL" ] && notified_critical=0
+done
