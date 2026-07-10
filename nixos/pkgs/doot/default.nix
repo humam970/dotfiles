e928a8cc5a91fb3c89aa4c72a89de03642513589
humam970 <humam.102001@gmail.com>
2026-07-10 19:44:02 +0300

    committed

--- nixos/pkgs/doot/default.nix
+++ nixos/pkgs/doot/default.nix
@@ -6,23 +6,23 @@
 }:
 
 buildGoModule rec {
-  pname = "doot";
-  version = "0.6.8";
+	pname = "doot";
+	version = "0.6.8";
+	vendorHash = "sha256-9h1EeNREmumRsaNHjD+NHkQtApiZuQZW/xj9lfRcQtE=";
+	doCheck = false;
 
-  src = fetchFromGitHub {
-    owner = "pol-rivero";
-    repo = "doot";
-    rev = version;
-    hash = lib.fakeHash;
-  };
+	src = fetchFromGitHub {
+		owner = "pol-rivero";
+		repo = "doot";
+		rev = version;
+		hash = "sha256-NZnga6KRYaBGgIoKGwskyy1jGYjT0qoMSQ6jh1Cjf5A=";
+	};
 
-  vendorHash = lib.fakeHash;
-
-  meta = with lib; {
-    description = "Dotfiles manager";
-    homepage = "https://github.com/pol-rivero/doot";
-    license = licenses.mit;
-    platforms = platforms.linux;
-    mainProgram = "doot";
-  };
+	meta = with lib; {
+		description = "Dotfiles manager";
+		homepage = "https://github.com/pol-rivero/doot";
+		license = licenses.mit;
+		platforms = platforms.linux;
+		mainProgram = "doot";
+	};
 }
